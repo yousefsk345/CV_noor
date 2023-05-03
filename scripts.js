5227497f7fc27a8914f07c77ee1ffc6277e69559
yousefsk345 <86665919+yousefsk345@users.noreply.github.com>
2023-05-03 16:15:37 +0300

    Update and rename p17.js to scripts.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function fun1(){
+   document.getElementById("information").innerHTML= "المعلومات الشخصية";
+   document.getElementById("details").innerHTML= "<ul><li>الاسم: نور مجدي سخل</li>   <li>رقم الجوال:0598745632</li> <li>تاريخ الميلاد:22-1-1990</li> <li>الجنسية: فلسطينية</li><li>الديانة:مسلمة</li><li>البريد الإلكتروني: noorsakhel9@gmail.com</li> <li>الحالة الاجتماعية: عزباء</li> <li>عنوان الاقامة: شارع التعاون، نابلس</li></ul>";
+}
+function fun2(){
+    document.getElementById("information").innerHTML="المؤهلات العلمية";
+    document.getElementById("details").innerHTML=" <b>شهادة ثانوية عامة فرع التكنولوجي - </b>مدرسة سمير سعد الدين 2008 <br> <b>بكالوريوس في أنظمة المعلومات الحاسوبية </b>- جامعة القدس المفتوحة 2013";
+}
+function fun3(){
+    document.getElementById("information").innerHTML="اللغات";
+    document.getElementById("details").innerHTML="<ul> <li><b>العربية:</b></li>مستوى القراءة:ممتاز <br>مستوى الكتابة:ممتاز <br>مستوى المحادثة:ممتاز <br><br><li><b>الانجليزية:</b></li>مستوى القراءة:ممتاز <br>مستوى الكتابة:ممتاز<br>مستوى المحادثة:جيد</ul>";
+}
+function fun4(){
+    window.location="https://www.qou.edu/";
+}
